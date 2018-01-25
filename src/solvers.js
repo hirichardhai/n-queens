9cade5687e9d837bbef527ee638fd9b10ec1acfb
hirichardhai <hirichardhai@gmail.com>
2018-01-24 20:12:02 -0800

    Finished adding one rook solution and pseudocoded for rook n solutions

--- src/solvers.js
+++ src/solvers.js
@@ -16,16 +16,50 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme
 
+  for (var i = 0; i < n; i++) {
+    var arr = [];
+    for (var j = 0; j < n; j++) {
+      arr.push(0);
+    }
+    arr[i] = 1;
+    solution.push(arr);
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var matrix = new Board(n);
+  
 
+  var innerRecursive = function(matrix, columnInserted) {
+
+    
+
+
+  };
+  /*
+    innerRecursive function (state of the matrix, columnToInsert) {
+      add a new rook to the matrix at (next row, columnToInsert)    
+
+      Base case:
+      check the validity of the matrix
+      check if we're at the bottom row
+        if yes and yes, solutionCount++
+
+      Recursive case:
+      iterate over the columns: for (i from 0 to n) {
+        call innerRecursive function with matrix, and the column where we're about to insert a rook
+          (* the next rook will be at (next row, column i) )
+      }
+
+    }
+  */
+  // call function
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
